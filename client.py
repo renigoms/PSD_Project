c038f382d6caa99b68e98c0ef7441a76905627e1
jonasr1 <yatosama924@gmail.com>
2025-01-28 18:56:31 -0300

    refactor: improve private message handling and apply clean code principles - Replaced '@' prefix with msg 'U' to identify private messages (-msg U username)

--- client.py
+++ client.py
@@ -1,48 +1,67 @@
 import socket
 from colorama import Style, Fore
 from threading import Thread
+from constants import REQUIRED_MESSAGE_PARTS
 
 
 class Client:
-    REQUIRED_PRIVATE_MESSAGE_PARTS = 2
 
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
 
     def run(self):
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            client_socket.connect((self.host, self.port))
-        except ConnectionRefusedError:
-            print(Fore.RED + '\nNão foi possível conectar ao servidor. Verifique se ele está ativo!' + Style.RESET_ALL)
+        client_socket = self._connect_to_server()
+        if not client_socket:
             return
+        username = Client._get_username()
+        if not username:
+            return
+        if not Client._authenticate_user(client_socket, username):
+            return
+        Client._start_message_threads(client_socket, username)
 
+    @staticmethod
+    def _get_username():
         username = input('\nDigite seu nome de usuário: ').strip()
         if not username:
             print(Fore.RED + 'Nome de usuário inválido. Encerrando conexão.' + Style.RESET_ALL)
-            return
+        return username
 
+    def _connect_to_server(self) -> socket.socket | None:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            try:
-                client_socket.send(username.encode('utf-8'))
-                response = client_socket.recv(1024).decode()
-                header = response[:10].strip()
-                message = response[10:]
-                if header == 'ERROR':
-                    print(Fore.YELLOW + f'Erro recebido do servidor: {message}' + Style.RESET_ALL)
-                    return
-                elif header == 'OK':
-                    print(Fore.GREEN + f'\n{message}' + Style.RESET_ALL)  # Mensagem de sucesso
-                else:
-                    print(Fore.RED + '\nResposta inesperada do servidor. Encerrando conexão.' + Style.RESET_ALL)
-                    return
-            except:
-                print(Fore.RED + 'Erro ao enviar o nome de usuário. Encerrando conexão.' + Style.RESET_ALL)
-                return
+            client_socket.connect((self.host, self.port))
+            return client_socket
+        except ConnectionRefusedError:
+            print(Fore.RED + '\nNão foi possível conectar ao servidor. Verifique se ele está ativo!' + Style.RESET_ALL)
+            return None
 
-            thread_receive = Thread(target=self.receive_message, args=[client_socket])
-            thread_send = Thread(target=self.send_messages, args=[client_socket, username])
+    @staticmethod
+    def _authenticate_user(client_socket: socket.socket, username: str) -> bool:
+        try:
+            client_socket.send(username.encode('utf-8'))
+            response = client_socket.recv(1024).decode()
+            header = response[:10].strip()
+            message = response[10:]
+            if header == 'ERROR':
+                print(Fore.YELLOW + f'Erro recebido do servidor: {message}' + Style.RESET_ALL)
+                return False
+            elif header == 'OK':
+                print(Fore.GREEN + f'\n{message}' + Style.RESET_ALL)  # Mensagem de sucesso
+                return True
+            else:
+                print(Fore.RED + '\nResposta inesperada do servidor. Encerrando conexão.' + Style.RESET_ALL)
+                return False
+        except Exception as e:
+            print(Fore.RED + f'Erro ao enviar o nome de usuário. Encerrando conexão. \n{e}' + Style.RESET_ALL)
+            return False
+
+    @staticmethod
+    def _start_message_threads(client_socket: socket.socket, username: str):
+        try:
+            thread_receive = Thread(target=Client._receive_message, args=[client_socket])
+            thread_send = Thread(target=Client._send_messages, args=[client_socket, username])
 
             thread_receive.start()
             thread_send.start()
@@ -53,7 +72,8 @@ class Client:
         except KeyboardInterrupt:
             print(Fore.YELLOW + '\nConexão interrompida manualmente. Encerrando...' + Style.RESET_ALL)
 
-    def receive_message(self, client_socket: socket.socket):
+    @staticmethod
+    def _receive_message(client_socket: socket.socket):
         while True:
             try:
                 message = client_socket.recv(1024).decode('utf-8')
@@ -66,20 +86,33 @@ class Client:
                 client_socket.close()
                 break
 
-    def send_messages(self, client_socket: socket.socket, username: str):
+    @staticmethod
+    def _send_messages(client_socket: socket.socket, username: str):
         try:
             while True:
                 message = input()
                 if not message.strip():
                     print(Fore.RED + 'Mensagem vazia. Digite algo válido.' + Style.RESET_ALL)
                     continue
-                if message.startswith('@') and len(message.split(' ', 1)) == self.REQUIRED_PRIVATE_MESSAGE_PARTS:
+                parts = message.split(' ', 3)  # Divide em até 4 partes: comando, "tag", username e mensagem
+                if len(parts) != REQUIRED_MESSAGE_PARTS or not message.startswith('-msg'):
+                    print(
+                        Fore.YELLOW
+                        + 'Formato inválido para mensagem. Use: -msg tag <usuário|grupo> <mensagem>'
+                        + '\nSubstitua tag por U mensagem privada G grupo'
+                        + Style.RESET_ALL
+                    )
+                    continue
+                if message.startswith('-msg') and parts[1].upper() == 'U':
                     client_socket.send(message.encode('utf-8'))
                     continue
-                if message.startswith('@'):
-                    print(Fore.YELLOW + 'Formato inválido para mensagem privada. Use: @username mensagem' + Style.RESET_ALL)
+                else:
+                    print(
+                        Fore.YELLOW
+                        + 'Formato inválido para mensagem privada. Use: -msg U <usuário> <mensagem>'
+                        + Style.RESET_ALL
+                    )
                     continue
-                client_socket.send(message.encode('utf-8'))
         except (ConnectionResetError, BrokenPipeError):
             print(Fore.RED + '\nErro ao enviar mensagem. Conexão encerrada!' + Style.RESET_ALL)
         finally:
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 from threading import Thread
 from colorama import Fore, Style
+from constants import REQUIRED_MESSAGE_PARTS
+from datetime import datetime
 
 
 class Server:
@@ -11,6 +13,10 @@ class Server:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def run(self):
+        self._start_server()
+        self._accept_connections()
+
+    def _start_server(self):
         try:
             self.server_socket.bind((self.host, self.port))
             self.server_socket.listen()
@@ -19,98 +25,124 @@ class Server:
             print(Fore.RED + f"\nErro ao iniciar o servidor: {e}" + Style.RESET_ALL)
             return
 
+    def _accept_connections(self):
         try:
             while True:
                 client_socket, address = self.server_socket.accept()
                 print(Fore.CYAN + f"Nova conexão de {address}" + Style.RESET_ALL)
-                Thread(target=self.handle_new_client, args=(client_socket, address)).start()
+                Thread(target=self._handle_new_client, args=(client_socket, address)).start()
         except KeyboardInterrupt:
             print(Fore.YELLOW + "\nServidor interrompido manualmente. Fechando conexões..." + Style.RESET_ALL)
         finally:
-            self.shutdown()
+            self._shutdown()
 
-    def handle_new_client(self, client_socket: socket.socket, address: tuple):
+    def _handle_new_client(self, client_socket: socket.socket, address: tuple):
         try:
-            name = client_socket.recv(1024).decode("utf-8").capitalize()
-            if not name:
-                client_socket.close()
+            username = self._receive_username(client_socket)
+            if not username:
                 return
-            if name in self.clients.values():
-                header = 'ERROR'.ljust(10)
-                message = 'Usuário já conectado'
-                client_socket.send((header + message).encode('utf-8'))
-                client_socket.close()
-                return
-            # Envia uma flag de sucesso para o cliente
-            header = 'OK'.ljust(10)  # Header indicando sucesso
-            message = 'Conexão estabelecida com sucesso!'
-            client_socket.send((header + message).encode('utf-8'))
-            self.clients[client_socket] = name
-            print(Fore.BLUE + f"{name} ({address}) conectou-se ao servidor." + Style.RESET_ALL)
+            Server._send_success_response(client_socket, 'Conexão estabelecida com sucesso!')
+            self.clients[client_socket] = username
+            print(Fore.BLUE + f"{username} ({address}) conectou-se ao servidor." + Style.RESET_ALL)
 
             # Mensagem de boas-vindas
-            self.broadcast(f"{name} entrou no chat.", client_socket)
+            self._broadcast(f"{username} entrou no chat.", client_socket)
 
             # Começa a tratar mensagens desse cliente
-            self.handle_client_messages(client_socket)
+            self._handle_client_messages(client_socket)
         except Exception as e:
             print(Fore.RED + f"Erro ao lidar com o cliente {address}: {e}" + Style.RESET_ALL)
 
-    def handle_client_messages(self, client_socket: socket.socket):
-        name = self.clients.get(client_socket, "Desconhecido")
+    def _receive_username(self, client_socket: socket.socket) -> str:
+        username = client_socket.recv(1024).decode('utf-8').capitalize()
+        if not username:
+            client_socket.close()
+            return ''
+        if username in self.clients.values():
+            Server._send_error_response(client_socket, 'Usuário já conectado')
+            client_socket.close()
+            return ''
+        return username
+
+    def _handle_client_messages(self, client_socket: socket.socket):
+        username = self.clients.get(client_socket, "Desconhecido")
         try:
             while True:
                 message = client_socket.recv(1024).decode("utf-8").strip()
+                if not message:
+                    continue
                 if message:
-                    if message.startswith('@'):
-                        recipient_name, msg = message.split(' ', 1)
-                        recipient_name = recipient_name[1:]
-                        self.send_private_message(recipient_name, f'{name}: {msg}', client_socket)
+                    if message.startswith('-msg U'):
+                        self._handle_private_message(client_socket, username, message)
+                        _, _, recipient_name, msg = message.split(' ', 3)
+                        # recipient_name = recipient_name[1:]
+                        self._handle_private_message(client_socket, recipient_name,msg)
                     else:
-                        broadcast_message = f"{name}: {message}"
+                        broadcast_message = f"{username}: {message}"
                         print(broadcast_message)
-                        self.broadcast(broadcast_message, client_socket)
+                        self._broadcast(broadcast_message, client_socket)
                 else:
-                    # Encerra a conexão se a mensagem estiver vazia
                     break
         except (ConnectionResetError, ConnectionAbortedError):
-            print(Fore.RED + f"Conexão perdida com {name}." + Style.RESET_ALL)
+            print(Fore.RED + f"Conexão perdida com {username}." + Style.RESET_ALL)
         finally:
-            self.remove_client(client_socket)
+            self._remove_client(client_socket)
 
-    def broadcast(self, message: str, sender_socket: socket.socket = None):
-        for client_socket in list(self.clients.keys()):
-            if client_socket != sender_socket:
-                try:
-                    client_socket.send(message.encode("utf-8"))
-                except (ConnectionResetError, ConnectionAbortedError):
-                    self.remove_client(client_socket)
+    def _handle_private_message(self, client_socket: socket.socket, sender_username: str, message: str):
+        parts = message.split(' ', 3)
+        if len(parts) == REQUIRED_MESSAGE_PARTS:
+            recipient_name = parts[2]
+            msg = parts[3]
+            formatted_message = f'({sender_username}, {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}): {msg}'
+            self._send_private_message(recipient_name, formatted_message, client_socket)
 
-    def remove_client(self, client_socket: socket.socket):
-        name = self.clients.pop(client_socket, "Desconhecido")
-        client_socket.close()
-        print(Fore.YELLOW + f"{name} desconectou-se do servidor." + Style.RESET_ALL)
-
-        # Mensagem de despedida
-        self.broadcast(f"{name} saiu do chat.", None)
-
-    def shutdown(self):
-        for client_socket in list(self.clients.keys()):
-            client_socket.close()
-        self.server_socket.close()
-        print(Fore.RED + "Servidor fechado." + Style.RESET_ALL)
-
-    def send_private_message(self, recipient_name: str, message: str, sender_socket: socket.socket):
+    def _send_private_message(self, recipient_name: str, message: str, sender_socket: socket.socket):
         for client_socket, client_name in self.clients.items():
             if client_name == recipient_name.capitalize():
                 try:
                     client_socket.send(message.encode('utf-8'))
                     sender_name = self.clients.get(client_socket, 'Desconhecido')
-                    print(Fore.YELLOW + f'Mensagem privada de {sender_name} para {recipient_name}: {message}' + Style.RESET_ALL)
+                    print(
+                        Fore.YELLOW
+                        + f'Mensagem privada de {sender_name} para {recipient_name}: {message}'
+                        + Style.RESET_ALL
+                    )
                 except (ConnectionResetError, ConnectionAbortedError):
-                    self.remove_client(client_socket)
+                    self._remove_client(client_socket)
                 return
         sender_socket.send((Fore.RED + f'Erro: {recipient_name} não encontrado' + Style.RESET_ALL).encode())
+
+    def _broadcast(self, message: str, sender_socket: socket.socket = None):
+        for client_socket in list(self.clients.keys()):
+            if client_socket != sender_socket:
+                try:
+                    client_socket.send(message.encode("utf-8"))
+                except (ConnectionResetError, ConnectionAbortedError):
+                    self._remove_client(client_socket)
+
+    def _remove_client(self, client_socket: socket.socket):
+        username = self.clients.pop(client_socket, "Desconhecido")
+        client_socket.close()
+        print(Fore.YELLOW + f"{username} desconectou-se do servidor." + Style.RESET_ALL)
+
+        # Mensagem de despedida
+        self._broadcast(f"{username} saiu do chat.", None)
+
+    def _shutdown(self):
+        for client_socket in list(self.clients.keys()):
+            client_socket.close()
+        self.server_socket.close()
+        print(Fore.RED + "Servidor fechado." + Style.RESET_ALL)
+
+    @staticmethod
+    def _send_success_response(client_socket: socket.socket, message: str):
+        header = 'OK'.ljust(10)
+        client_socket.send((header + message).encode('utf-8'))
+
+    @staticmethod
+    def _send_error_response(client_socket: socket.socket, mensagem: str):
+        header = 'ERROR'.ljust(10)
+        client_socket.send((header + mensagem).encode('utf-8'))
 
 
 if __name__ == '__main__':
