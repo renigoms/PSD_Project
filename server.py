a7d809f29770c7317b127574ba64b70920506ea2
jonasr1 <yatosama924@gmail.com>
2025-01-29 16:23:47 -0300

    feat: add command to list all users (-listarusuarios)

--- server.py
+++ server.py
@@ -65,43 +65,42 @@ class Server:
         return username
 
     def _handle_client_messages(self, client_socket: socket.socket):
+        msg = ''
         username = self.clients.get(client_socket, "Desconhecido")
         try:
             while True:
                 message = client_socket.recv(1024).decode("utf-8").strip()
                 if not message:
                     continue
-                if message:
-                    if message.startswith('-msg U'):
-                        self._handle_private_message(client_socket, username, message)
-                        _, _, recipient_name, msg = message.split(' ', 3)
-                        # recipient_name = recipient_name[1:]
-                        self._handle_private_message(client_socket, recipient_name,msg)
+                if message.startswith('-msg') and REQUIRED_MESSAGE_PARTS == len((parts := message.split(' ', 3))):
+                    command, tag, recipient_name, msg = parts
+                    if command == '-msg' and tag.upper() == 'U':
+                        self._handle_private_message(recipient_name, sender_username=username,
+                                                     sender_socket=client_socket, message=msg)
                     else:
                         broadcast_message = f"{username}: {message}"
                         print(broadcast_message)
                         self._broadcast(broadcast_message, client_socket)
+                elif message == '-listarusuarios':
+                    self._send_user_list(client_socket)
                 else:
-                    break
-        except (ConnectionResetError, ConnectionAbortedError):
+                    continue
+        except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError):
             print(Fore.RED + f"Conexão perdida com {username}." + Style.RESET_ALL)
         finally:
             self._remove_client(client_socket)
 
-    def _handle_private_message(self, client_socket: socket.socket, sender_username: str, message: str):
-        parts = message.split(' ', 3)
-        if len(parts) == REQUIRED_MESSAGE_PARTS:
-            recipient_name = parts[2]
-            msg = parts[3]
-            formatted_message = f'({sender_username}, {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}): {msg}'
-            self._send_private_message(recipient_name, formatted_message, client_socket)
+    def _handle_private_message(self, recipient_name: str, sender_username: str,
+                                sender_socket: socket.socket, message: str):
+        formatted_message = f'({sender_username}, {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}): {message}'
+        self._send_private_message(sender_username, recipient_name,
+                                   sender_socket=sender_socket, message=formatted_message)
 
-    def _send_private_message(self, recipient_name: str, message: str, sender_socket: socket.socket):
+    def _send_private_message(self, sender_name: str, recipient_name: str, sender_socket: socket.socket, message: str):
         for client_socket, client_name in self.clients.items():
             if client_name == recipient_name.capitalize():
                 try:
-                    client_socket.send(message.encode('utf-8'))
-                    sender_name = self.clients.get(client_socket, 'Desconhecido')
+                    Server.send_message_safe(client_socket, message)
                     print(
                         Fore.YELLOW
                         + f'Mensagem privada de {sender_name} para {recipient_name}: {message}'
@@ -116,7 +115,7 @@ class Server:
         for client_socket in list(self.clients.keys()):
             if client_socket != sender_socket:
                 try:
-                    client_socket.send(message.encode("utf-8"))
+                    Server.send_message_safe(client_socket, message)
                 except (ConnectionResetError, ConnectionAbortedError):
                     self._remove_client(client_socket)
 
@@ -143,6 +142,23 @@ class Server:
     def _send_error_response(client_socket: socket.socket, mensagem: str):
         header = 'ERROR'.ljust(10)
         client_socket.send((header + mensagem).encode('utf-8'))
+
+    @staticmethod
+    def send_message_safe(client_socket: socket.socket, message):
+        try:
+            client_socket.send(message.encode('utf-8'))
+        except (BrokenPipeError, ConnectionResetError) as e:
+            print(Fore.RED + f"Erro ao enviar mensagem: {e}" + Style.RESET_ALL)
+            # Aqui você pode fechar a conexão ou remover o cliente da lista
+            client_socket.close()
+
+    def _send_user_list(self, client_socket: socket.socket):
+        """Envia a lista de todos os usuários conectados para o cliente"""
+        users = '\n'.join(self.clients.values())
+        try:
+            client_socket.send((Fore.CYAN + 'Usuários online:\n' + users + Style.RESET_ALL).encode('utf-8'))
+        except (ConnectionResetError, ConnectionAbortedError):
+            self._remove_client(client_socket)
 
 
 if __name__ == '__main__':
